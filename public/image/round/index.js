4c9b01c7b522807741929bde77af287dd089ba10
hta393939 <hta393939@gmail.com>
2024-05-11 13:55:51 +0900

    left and right

--- public/image/round/index.js
+++ public/image/round/index.js
@@ -25,54 +25,63 @@ class Misc {
     const c = canvas.getContext('2d');
     const data = c.getImageData(0, 0, w, h);
 
-    const center = [440, 71];
+    const center = [440, 71]; // 1607, 71
     const router = 37;
+    const rinner = 0;
     const ps = [
-      [0, 0, 0, 255, 70],
-      [0, 0, 0, 255, 130],
+      [0, 0, 0, 255, 70], // 角度
+      [0, 0, 0, 255, 130], // 角度
     ];
 
-    for (let y = 0; y < h; ++y) {
-      for (let x = 0; x < w; ++x) {
-        const dx = x - center[0];
-        const dy = y - center[1];
-        const d = Math.sqrt(dx * dx + dy * dy);
-        const ang = Math.atan2(-dy, dx);
-        const deg = ang * 180 / Math.PI;
-        if (deg < ps[0][4] || deg > ps[1][4]) {
-          continue;
+    for (let lr = 0; lr < 2; ++lr) {
+      if (lr === 1) {
+        center[0] = 2047 - center[0];
+        ps[0][4] = 180 - ps[0][4];
+        ps[1][4] = 180 - ps[1][4];
+      }
+
+      for (let y = 0; y < h; ++y) {
+        for (let x = 0; x < w; ++x) {
+          const dx = x - center[0];
+          const dy = y - center[1];
+          const d = Math.sqrt(dx * dx + dy * dy);
+          const ang = Math.atan2(-dy, dx);
+          const deg = ang * 180 / Math.PI;
+          if (deg < ps[0][4] || deg > ps[1][4]) {
+            continue;
+          }
+          if (d > router || rinner > d) {
+            continue;
+          }
+
+          for (let i = 0; i < 2; ++i) {
+            const pang = ps[i][4] * Math.PI / 180;
+            let px = Math.round(center[0] + Math.cos(pang) * d);
+            let py = Math.round(center[1] - Math.sin(pang) * d);
+            const offset = (px + h * py) * 4;
+            ps[i][0] = data.data[offset];
+            ps[i][1] = data.data[offset+1];
+            ps[i][2] = data.data[offset+2];
+          }
+
+          const offset = (x + h * y) * 4;
+
+          let t = (deg - 70) / (130 - 70);
+
+          let r = _lerp(ps[0][0], ps[1][0], t);
+          let g = _lerp(ps[0][1], ps[1][1], t);
+          let b = _lerp(ps[0][2], ps[1][2], t);
+          let a = 255;
+
+          r = Math.max(0, Math.min(r, 255));
+          g = Math.max(0, Math.min(g, 255));
+          b = Math.max(0, Math.min(b, 255));
+
+          data.data[offset+0] = r;
+          data.data[offset+1] = g;
+          data.data[offset+2] = b;
+          data.data[offset+3] = a;
         }
-        if (d > router) {
-          continue;
-        }
-
-        for (let i = 0; i < 2; ++i) {
-          const pang = ps[i][4] * Math.PI / 180;
-          let px = Math.round(center[0] + Math.cos(pang) * d);
-          let py = Math.round(center[1] - Math.sin(pang) * d);
-          const offset = (px + h * py) * 4;
-          ps[i][0] = data.data[offset];
-          ps[i][1] = data.data[offset+1];
-          ps[i][2] = data.data[offset+2];
-        }
-
-        const offset = (x + h * y) * 4;
-
-        let t = (deg - 70) / (130 - 70);
-
-        let r = _lerp(ps[0][0], ps[1][0], t);
-        let g = _lerp(ps[0][1], ps[1][1], t);
-        let b = _lerp(ps[0][2], ps[1][2], t);
-        let a = 255;
-
-        r = Math.max(0, Math.min(r, 255));
-        g = Math.max(0, Math.min(g, 255));
-        b = Math.max(0, Math.min(b, 255));
-
-        data.data[offset+0] = r;
-        data.data[offset+1] = g;
-        data.data[offset+2] = b;
-        data.data[offset+3] = a;
       }
     }
     c.putImageData(data, 0, 0);
