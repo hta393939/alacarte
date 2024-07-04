73a0fc3b0716a3f844be72364cdca4786759fc6f
hta393939 <hta393939@gmail.com>
2024-07-04 20:18:48 +0900

    commit

--- public/image/imagetool/index.js
+++ public/image/imagetool/index.js
@@ -5,6 +5,16 @@
 class Misc {
   constructor() {
     this.STORAGE = 'imagetool';
+
+    this.param = {
+      scale: 11,
+      cellx: 0,
+      celly: 0,
+      cellw: 1,
+      cellh: 1,
+      x: 0,
+      y: 0,
+    };
   }
 
   async initialize() {
@@ -26,7 +36,7 @@ class Misc {
     for (let i = 0; i < h; ++i) {
       for (let j = 0; j < w; ++j) {
         let ft = (j + i * w) * 4;
-        let r = dat.data[ft];
+        let r = dat.data[ft  ];
         let g = dat.data[ft+1];
         let b = dat.data[ft+2];
         let a = dat.data[ft+3];
@@ -41,7 +51,7 @@ class Misc {
           return Math.max(0, Math.min(255, Math.round(v)));
         });
 
-        dat.data[ft] = cols[0];
+        dat.data[ft  ] = cols[0];
         dat.data[ft+1] = cols[1];
         dat.data[ft+2] = cols[2];
         dat.data[ft+3] = cols[3];
@@ -151,11 +161,7 @@ class Misc {
   }
 
   loadSetting() {
-    const param = {
-      foo: 1,
-      bar: 2,
-      baz: 3,
-    };
+    const param = this.param;
     try {
       const obj = JSON.parse(localStorage.getItem(this.STORAGE));
       for (const key in obj) {
@@ -169,13 +175,9 @@ class Misc {
   }
 
   saveSetting() {
-    const param = {
-      foo: 1,
-      bar: 2,
-      baz: 3,
-    };
+    const param = this.param;
     try {
-      for (const key of param) {
+      for (const key in param) {
         const el = document.getElementById(key);
         if (!el) {
           continue;
@@ -190,9 +192,19 @@ class Misc {
       console.warn('saveSetting', e.message);
     }
     localStorage.setItem(this.STORAGE, JSON.stringify(param));
+    return param;
   }
 
   setListener() {
+    {
+      const el = document;
+      el?.addEventListener('dragover', ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        ev.dataTransfer.dropEffect = 'none';
+      });
+    }
+
     {
       const el = document.getElementById('convcolor');
       el.addEventListener('dragover', ev => {
@@ -226,14 +238,14 @@ class Misc {
       });
     }
 
-    for (const k of [
-      'scale', 'cellx', 'celly', 'cellw',
-    ]) {
+    for (const k in this.param) {
       const el = document.getElementById(`${k}`);
       const viewel = document.getElementById(`${k}view`);
       const _update = () => {
         this[k] = Number.parseFloat(el.value);
         viewel.textContent = this[k];
+
+        this.drawFrame();
       };
       el?.addEventListener('input', () => {
         _update();
@@ -249,6 +261,99 @@ class Misc {
       });
     }
 
+    {
+      const el = document.getElementById('act');
+      el?.addEventListener('click', () => {
+        this.act();
+      });
+    }
+
+  }
+
+  act() {
+    console.log('act called');
+    const param = this.saveSetting();
+    const side = param.scale;
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    const src = document.getElementById('maincanvas');
+    const w = src.width;
+    const h = src.height;
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    const canvas = document.getElementById('subcanvas');
+    canvas.width = w;
+    canvas.height = h;
+    const c = canvas.getContext('2d');
+    c.drawImage(src, 0, 0);
+    const data = c.getImageData(0, 0, w, h);
+    const _sum = (inx, iny, inw, inh) => {
+      const cols = [0, 0, 0, 0];
+      for (let y = 0; y < inh; ++y) {
+        for (let x = 0; x < inw; ++x) {
+          const ft = (x + inx + w * (y + iny)) * 4;
+          for (let i = 0; i < 4; ++i) {
+            cols[i] += data.data[ft+i];
+          }
+        }
+      }
+      const k = (inw * inh > 0) ? 1 / (inw * inh) : 0;
+      return cols.map(v => v * k);
+    };
+    const _set = (inx, iny, inw, inh, incols) => {
+      for (let y = 0; y < inh; ++y) {
+        for (let x = 0; x < inw; ++x) {
+          const ft = (x + inx + w * (y + iny)) * 4;
+          for (let i = 0; i < 4; ++i) {
+            data.data[ft+i] = incols[i];
+          }
+        }
+      }
+    };
+
+    for (let y = 0; y < param.cellh; y++) {
+      for (let x = 0; x < param.cellw; ++x) {
+        let offsetX = (x + param.cellx) * side;
+        let offsetY = (y + param.celly) * side;
+        const cols = _sum(
+          offsetX, offsetY,
+          side, side);
+        _set(offsetX, offsetY,
+          side, side, cols);
+      }
+    }
+    c.putImageData(data, 0, 0);
+    console.log('act leave');
+  }
+
+  drawFrame() {
+    console.log('drawFrame called');
+    const param = this.saveSetting();
+    const side = param.scale;
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    const src = document.getElementById('maincanvas');
+    const w = src.width;
+    const h = src.height;
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    const canvas = document.getElementById('subcanvas');
+    canvas.width = w;
+    canvas.height = h;
+    const c = canvas.getContext('2d');
+    c.drawImage(src, 0, 0);
+    {
+      c.lineWidth = 1.5;
+      c.strokeStyle = 'red';
+      let x = param.cellx * side;
+      let y = param.celly * side;
+      c.strokeRect(x, y,
+        param.cellw * side, param.cellh * side);
+    }
   }
 
 }
