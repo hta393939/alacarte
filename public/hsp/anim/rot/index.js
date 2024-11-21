70a7b7b0c1e22af65055b1c7dc51ffabe77fc6d1
hta393939 <hta393939@gmail.com>
2024-11-21 20:22:03 +0900

    relative position

--- public/hsp/anim/rot/index.js
+++ public/hsp/anim/rot/index.js
@@ -47,19 +47,74 @@ class Misc {
         `ude_R_`, `arm_R_`, `momo_R_`, `asi_R_`,
       ];
 
+      /*
+      bone_pos(0,0) = 0.000000, 0.000000, 0.000000,
+      bone_names(1) = "hip"
+      bone_pos(0,1) = 0.000000, 20.000000, 0.000000,
+      bone_names(2) = "chest"
+      bone_pos(0,2) = 0.000000, 25.000000, 0.000000,
+      bone_names(3) = "kata_R_"
+      bone_pos(0,3) = -30.000000, 15.000000, 0.000000,
+      bone_names(4) = "ude_R_"
+      bone_pos(0,4) = 0.000000, -25.000000, 0.000000,
+      bone_names(5) = "kata_L_"
+      bone_pos(0,5) = 30.000000, 15.000000, -0.000008,
+      bone_names(6) = "ude_L_"
+      bone_pos(0,6) = 0.000000, -25.000000, 0.000000,
+      bone_names(7) = "head"
+      bone_pos(0,7) = 0.000000, 35.000000, -30.000000,
+      bone_names(8) = "momo_L_"
+      bone_pos(0,8) = 25.000000, 0.000000, 0.000000,
+      bone_names(9) = "asi_L_"
+      bone_pos(0,9) = 0.000000, -10.000000, 0.000000,
+      bone_names(10) = "momo_R_"
+      bone_pos(0,10) = -25.000000, 0.000000, 0.000000,
+      bone_names(11) = "asi_R_"
+      bone_pos(0,11) = 0.000000, -10.000000, 0.000000,
+*/
+const rel = {
+"XSBone_000": [0.000000, 0.000000, 0.000000,],
+"hip": [ 0.000000, 20.000000, 0.000000,],
+"chest": [
+ 0.000000, 25.000000, 0.000000,],
+"kata_R_": [
+-30.000000, 15.000000, 0.000000,],
+"ude_R_": [
+ 0.000000, -25.000000, 0.000000,],
+"kata_L_": [
+ 30.000000, 15.000000, -0.000008,],
+"ude_L_": [
+ 0.000000, -25.000000, 0.000000,],
+"head": [
+ 0.000000, 35.000000, -30.000000,],
+"momo_L_": [
+ 25.000000, 0.000000, 0.000000,],
+"asi_L_": [
+ 0.000000, -10.000000, 0.000000,],
+"momo_R_": [
+ -25.000000, 0.000000, 0.000000,],
+"asi_R_": [
+0.000000, -10.000000, 0.000000,]
+};
+
+
       for (let i = 0; i < targets.length; ++i) {
         const ch = new Channel();
         chs.push(ch);
 
         const targetName = targets[i];
         ch.targetId = targetName;
+
+        const relp = rel[targetName];
+        let p = relp ? [...relp] : [0, 0, 0];
+
         if (targetName !== 'head') {
           for (let j = 0; j <= 1; ++j) {
             let msec = Math.floor(j * 1000 * sec);
             let q = [0, 0, 0, 1];
   
             ch.keytimes.push(msec);
-            ch.values.push(...q, 0, 0, 0);
+            ch.values.push(...q, ...p);
           }
           continue;
         }
@@ -84,7 +139,7 @@ class Misc {
           q = q.map(v => { const r10 = 10 ** 6; return Math.ceil(v * r10) / r10; });
 
           ch.keytimes.push(msec);
-          ch.values.push(...q, 0, 0, 0);
+          ch.values.push(...q, ...p);
         }
 
       }
@@ -122,13 +177,13 @@ class Misc {
       ael.appendChild(chel);
 
 
-      const attribel = document.createElement('targetAttrib');
-      attribel.textContent = `16 TARGET_ROTATE_TRANSLATE`;
-      chel.appendChild(attribel);
-
       const targetel = document.createElement('targetId');
       targetel.textContent = ch.targetId;
       chel.appendChild(targetel);
+
+      const attribel = document.createElement('targetAttrib');
+      attribel.textContent = `16 ANIMATE_ROTATE_TRANSLATE`;
+      chel.appendChild(attribel);
 
       const kel = document.createElement('keytimes');
       kel.setAttribute('count', `${ch.keytimes.length}`);
@@ -168,7 +223,7 @@ class Misc {
     let result = intext;
     const pairs = [
       {target: 'Animations'},
-      {target: 'Animation'},
+      {target: 'Animation', reF: /<animation(?![cCsS])/g, },
       {target: 'AnimationChannel'},
       {target: 'targetAttrib'},
       {target: 'targetId'},
@@ -178,16 +233,18 @@ class Misc {
     ];
     for (const pair of pairs) {
       {
-        const search = new RegExp(`<${pair.target.toLocaleLowerCase()}`, 'g');
-        const rep = `<${pair.target}`;
+        const search = pair.target.reF || new RegExp(`(?<=</?)${pair.target.toLocaleLowerCase()}(?=[\\s>])`, 'g');
+        const rep = `${pair.target}`;
+        // キャプチャできるか??
         result = result.replaceAll(search, rep);
       }
-      {
+      /*{
         const search = new RegExp(`</${pair.target.toLocaleLowerCase()}>`, 'g');
         const rep = `</${pair.target}>`;
         result = result.replaceAll(search, rep);
-      }
+      }*/
     }
+    console.log('replaceAll', result);
 
     return result;
   }
