b6959633fbad9d08edc0745905f5198d2d4c0f8b
hta393939 <hta393939@gmail.com>
2025-01-18 17:53:56 +0900

    WIP: wasm

---
+++ public/em/hello/main.cpp
@@ -0,0 +1,27 @@
+#include <emscripten.h>
+#include <wasm_simd128.h>
+
+EMSCRIPTEN_KEEPALIVE
+extern "C" int sum(int pointer, int num) {
+  auto p = (float*)pointer;
+
+  int loopNum = num >> 2;
+  v128_t coeff = wasm_f32x4_const(2.0f, 2.0f, 2.0f, 1.0f);
+  for (int i = 0; i < loopNum; ++i) {
+    v128_t v0 = wasm_v128_load(p);
+    v128_t v1 = wasm_f32x4_mul(v0, coeff);
+    wasm_v128_store(p, v1);
+    p += 4;
+  }
+  return loopNum;
+}
+
+EMSCRIPTEN_KEEPALIVE
+extern "C" int add(int a, int b) {
+  return (a + b);
+}
+
+int main() {
+  return 1;
+}
+
