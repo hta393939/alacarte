1a02c6eb3b844fb705fabbc7fd2aef9991b3facc
hta393939 <hta393939@gmail.com>
2025-02-01 11:50:46 +0900

    WIP: build

--- public/hsp/cmp/_em1.sh
+++ public/hsp/cmp/_em1.sh
@@ -4,5 +4,6 @@ docker run \
   -v ${PWD}:/src \
   emscripten/emsdk \
   emcc --bind -o main.html \
+    -DHSPEMSCRIPTEN \
     -s EXPORTED_FUNCTIONS="['_main','_malloc','_free']" \
     hspcmp/emscripten/hspcmp3.cpp
