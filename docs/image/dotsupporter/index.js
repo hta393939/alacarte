b9f070b11df82bc92bd972985fc18afc589c0c7e
hta393939 <hta393939@gmail.com>
2025-12-14 11:22:37 +0900

    commit

---
+++ docs/image/dotsupporter/index.js
@@ -0,0 +1,134 @@
+/**
+ * 不透明の絵をscale倍でドット絵拡大する
+ */
+class Misc {
+  constructor() {
+    /**
+     * UIで値を変更する
+     */
+    this.scale = 4;
+    this.cellx = 0;
+    this.celly = 0;
+    this.cellw = 64;
+    this.cellh = 64;
+  }
+
+  async initialize() {
+    this.setListener();
+  }
+
+  /**
+   * 
+   * @param {HTMLCanvasElement} src 
+   */
+  scaleImage(src) {
+    const scale = this.scale;
+
+    const cellx = this.cellx;
+    const celly = this.celly;
+    const cellw = this.cellw;
+    const cellh = cellw;
+
+    /**
+     * 入力画像の幅
+     */
+//        const w = src.width;
+//        const h = src.height;
+    const context = src.getContext('2d');
+    /**
+     * 書き出し先
+     * @type {HTMLCanvasElement}
+     */
+    const canvas = document.getElementById('subcanvas');
+    const c = canvas.getContext('2d');
+    canvas.width = cellw * scale;
+    canvas.height = cellh * scale;
+    const cx = cellx * cellw;
+    const cy = celly * cellh;
+    console.log(cx, cy, cellw, cellh);
+    const dat = context.getImageData(cx, cy, cellw, cellh);
+
+    let backs = [-1, -1, -1];
+    if (true) { // 左上を背景色として採用する
+      let ft = (0 + 0 * 0) * 4;
+      backs[0] = dat.data[ft];
+      backs[1] = dat.data[ft+1];
+      backs[2] = dat.data[ft+2];
+    }
+
+    for (let i = 0; i < cellw; ++i) {
+      for (let j = 0; j < cellw; ++j) {
+        let ft = (j + i * cellw) * 4;
+        let x = j * scale;
+        let y = i * scale;
+        let r = dat.data[ft];
+        let g = dat.data[ft+1];
+        let b = dat.data[ft+2];
+        let a = dat.data[ft+3];
+        if (r === backs[0] && g === backs[1] && b === backs[2]) {
+          a = 0;
+        }
+        c.fillStyle = `rgba(${r}, ${g}, ${b}, ${a / 255})`;
+        c.fillRect(x, y, scale, scale);
+      }
+    }
+  }
+
+/**
+ * 
+ * @param {File} file 
+ */
+  async parseImage(file) {
+    const img = new Image();
+    img.addEventListener('load', () => {
+/**
+ * @type {HTMLCanvasElement}
+ */
+      const canvas = document.getElementById('maincanvas');
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const c = canvas.getContext('2d');
+      c.drawImage(img, 0, 0);
+      this.scaleImage(canvas);
+    });
+    img.src = URL.createObjectURL(file);
+  }
+
+  setListener() {
+    {
+      const el = window;
+      el.addEventListener('dragover', ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        ev.dataTransfer.dropEffect = 'copy';
+      });
+      el.addEventListener('drop', ev => {
+        ev.preventDefault();
+        ev.stopPropagation();
+        ev.dataTransfer.dropEffect = 'copy';
+        this.parseImage(ev.dataTransfer.files[0]);
+      });
+    }
+
+    for (const k of [
+      'scale', 'cellx', 'celly', 'cellw',
+    ]) {
+      const el = document.getElementById(`${k}`);
+      const viewel = document.getElementById(`${k}view`);
+      const _update = () => {
+        this[k] = Number.parseFloat(el.value);
+        viewel.textContent = this[k];
+      };
+      el?.addEventListener('input', () => {
+        _update();
+      });
+      _update();
+    }
+
+  }
+
+}
+
+const misc = new Misc();
+misc.initialize();
+
