806b3f8fb45964b856eaa53ac33539503038cb6a
hta393939 <hta393939@gmail.com>
2026-03-24 19:55:28 +0900

    WIP: part

--- public/lib/hsp/gpb.js
+++ public/lib/hsp/gpb.js
@@ -18,7 +18,6 @@ export class GpbNode {
   }
 }
 
-
 export class GpbAttribute {
   static TYPE_POSITION = 0;
   static TYPE_NORMAL = 1;
@@ -28,6 +27,16 @@ export class GpbAttribute {
   constructor() {
     this.type = GpbAttribute.TYPE_POSITION;
     this.num = 3;
+  }
+}
+
+export class GpbVertex {
+  constructor() {
+    this.p = [0, 0, 0];
+    this.n = [0, 0, 1];
+    this.uv = [0.5, 0.5];
+    this.weights = [1, 0, 0, 0];
+    this.joints = [0, 0, 0, 0];
   }
 }
 
@@ -41,18 +50,47 @@ export class GpbPart {
      */
     this.attrs = [];
 
+    /**
+     * @type {GpbVertex[]}
+     */
+    this.vts = [];
+
     this.center = [0, 0, 0];
-    this.posmin = [0, 0, 0];
-    this.posmax = [0, 0, 0];
-    this.radius = 1;
+    this.posmin = [99999, 99999, 99999];
+    this.posmax = [-99999, -99999, -99999];
+    this.radius = 0;
+
+    this.indices = [];
+  }
+
+  compute() {
+    const num = this.vts.length;
+    if (num === 0) {
+      return;
+    }
+    for (const v of this.vts) {
+      for (let j = 0; j < 3; ++j) {
+        const val = v.p[j];
+        this.posmax[j] = Math.max(this.posmax[j], val);
+        this.posmin[j] = Math.min(this.posmin[j], val);
+        this.center[j] += val;
+      }
+    }
+    let sum = 0;
+    for (let j = 0; j < 3; ++j) {
+      this.center[j] /= num;
+      sum += (this.posmax[j] - this.center[j]) ** 2;
+    }
+    this.radius = Math.sqrt(sum);
   }
 }
 
 export class GpbTable {
-  static TYPE_NODENODE = 1;
-  static TYPE_NODEJOINT = 2;
-  static TYPE_MESH = 0;
-  static TYPE_ANIMATION = 32;
+  static TYPE_SCENE = 1;
+  static TYPE_NODE = 2;
+  static TYPE_ANIMATION = 4;
+  static TYPE_MESH = 34;
+  static TYPE_ANIMATIONS = 3;
   static TYPE_FONT = 128;
   constructor() {
     this.type = GpbTable.TYPE_NODENODE;
@@ -112,11 +150,16 @@ export class GpbExport extends Gpb {
    * 
    * @param {DataView} p 
    * @param {number} c 
-   * @param {string} text 
+   * @param {string} text ascii
    */
   writestr(p, c, text) {
-
-    return 0;
+    const buf = new TextEncoder().encode(text);
+    const num = buf.byteLength;
+    p.setUint32(c, num, true);
+    for (let i = 0; i < num; ++i) {
+      p.setUint8(c + 4 + i, buf[i]);
+    }
+    return 4 + num;
   }
 
   /**
@@ -179,13 +222,34 @@ export class GpbExport extends Gpb {
 
   /**
    * 
+   * @param {DataView} p
+   * @param {number} c
    * @param {GpbPart} part 
    */
-  writePart(part) {
+  writePart(p, c, part) {
+    let offset = 0;
 
+    // 頂点ごと?
+    // 属性ごと?
+
+    offset += this.writefs(p, c + offset, part.posmin);
+    offset += this.writefs(p, c + offset, part.posmax);
+    offset += this.writefs(p, c + offset, part.center);
+    offset += this.writefs(p, c + offset, [part.radius]);
+    return offset;
   }
 
-  writeAnimation(anim) {
+  /**
+   * 
+   * @param {DataView} p 
+   * @param {number} c 
+   * @param {GpbAnimation} anim 
+   */
+  writeAnimation(p, c, anim) {
+    let offset = 0;
+    offset += this.writestr(p, c, anim.name);
+
+    return offset;
   }
 
   make() {
@@ -195,11 +259,12 @@ export class GpbExport extends Gpb {
 
     { // ヘッダ
       this.c += this.write8s(p, this.c,
-        [0, 0, 0, 0, 1, 5]
+        [0xAB, 0x47, 0x50, 0x42, 0xBB, 0x0D, 0x0A, 0x1A, 0x0A,  1, 5]
       );
     }
     { // テーブル
       const num = this.tables.length;
+      this.c += this.write32s(p, this.c, [num]);
       for (let i = 0; i < num; ++i) {
         const table = this.tables[i];
         table._infile = this.c;
