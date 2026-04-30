358e075fb2302d29e1c1bb1335f69a7db0c33f08
hta393939 <hta393939@gmail.com>
2026-04-30 14:53:43 +0900

    WIP: apriltag

---
+++ docs/misc/april/std/video_process.js
@@ -0,0 +1,83 @@
+import * as Comlink from "https://unpkg.com/comlink/dist/esm/comlink.mjs";
+
+//import * as Base64 from "./base64.js";
+
+var detections=[];
+
+window.onload = (event) => {
+  init();
+
+  loadImg('saved_det');
+}
+
+async function init() {
+  // WebWorkers use `postMessage` and therefore work with Comlink.
+  const Apriltag = Comlink.wrap(new Worker("apriltag.js"));
+
+  // must call this to init apriltag detector; argument is a callback for when the detector is ready
+  window.apriltag = await new Apriltag(Comlink.proxy(() => {
+
+    // set camera info; we must define these according to the device and image resolution for pose computation
+    //window.apriltag.set_camera_info(double fx, double fy, double cx, double cy)
+
+    window.apriltag.set_tag_size(5, .5);
+
+    // start processing frames
+    window.requestAnimationFrame(process_frame);
+  }));
+}
+
+async function process_frame() {
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  let ctx = canvas.getContext("2d");
+
+  let imageData;
+  try {
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
+  } catch (err) {
+    console.log("Failed to get video frame. Video not started ?");
+    setTimeout(process_frame, 500); // try again in 0.5 s
+    return;
+  }
+  let imageDataPixels = imageData.data;
+  let grayscalePixels = new Uint8Array(ctx.canvas.width * ctx.canvas.height); // this is the grayscale image we will pass to the detector
+
+  for (var i = 0, j = 0; i < imageDataPixels.length; i += 4, j++) {
+    let grayscale = Math.round((imageDataPixels[i] + imageDataPixels[i + 1] + imageDataPixels[i + 2]) / 3);
+    grayscalePixels[j] = grayscale; // single grayscale value
+    imageDataPixels[i] = grayscale;
+    imageDataPixels[i + 1] = grayscale;
+    imageDataPixels[i + 2] = grayscale;
+  }
+  ctx.putImageData(imageData, 0, 0);
+
+  // draw previous detection
+  detections.forEach(det => {
+    // draw tag borders
+    ctx.beginPath();
+      ctx.lineWidth = "5";
+      ctx.strokeStyle = "blue";
+      ctx.moveTo(det.corners[0].x, det.corners[0].y);
+      ctx.lineTo(det.corners[1].x, det.corners[1].y);
+      ctx.lineTo(det.corners[2].x, det.corners[2].y);
+      ctx.lineTo(det.corners[3].x, det.corners[3].y);
+      ctx.lineTo(det.corners[0].x, det.corners[0].y);
+      ctx.font = "bold 20px Arial";
+      var txt = ""+det.id;
+      ctx.fillStyle = "blue";
+      ctx.textAlign = "center";
+      ctx.fillText(txt, det.center.x, det.center.y+5);
+    ctx.stroke();
+  });
+
+  // detect aprilTag in the grayscale image given by grayscalePixels
+  detections = await apriltag.detect(grayscalePixels, ctx.canvas.width, ctx.canvas.height);
+
+//  if (detections.length > 0) {
+
+  window.requestAnimationFrame(process_frame);
+}
+
