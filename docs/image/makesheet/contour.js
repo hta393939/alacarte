36f8c9f0eb0674fdf50b4986cd1bf8338f48e85f
hta393939 <hta393939@gmail.com>
2026-05-02 14:48:11 +0900

    WIP: connect

--- docs/image/makesheet/contour.js
+++ docs/image/makesheet/contour.js
@@ -21,13 +21,38 @@ export class Edge {
 
     /**
      * 向き有り
+     * in: 時計周りの場合の内側の方向
      */
     this.withDirs = [
-      {dir: Edge.DIR_LEFT, in: Edge.DIR_UP, enable: false},
-      {dir: Edge.DIR_RIGHT, in: Edge.DIR_DOWN, enable: false},
+      {dir: Edge.DIR_LEFT, in: Edge.DIR_UP, enable: false, passed: false},
+      {dir: Edge.DIR_RIGHT, in: Edge.DIR_DOWN, enable: false, passed: false},
     ];
 
   }
+
+  /**
+   * 
+   * @param {number} side LEFT or UP 
+   * @param {boolean} e0 上または左
+   * @param {boolean} e1 下または右
+   */
+  set(side, e0, e1) {
+    this.side = side;
+    if (side === Edge.DIR_LEFT) {
+      this.withDirs = [
+        {dir: Edge.DIR_DOWN, in: Edge.DIR_LEFT, enable: false, passed: false},
+        {dir: Edge.DIR_UP, in: Edge.DIR_RIGHT, enable: false, passed: false},
+      ];
+    } else {
+      this.withDirs = [
+        {dir: Edge.DIR_LEFT, in: Edge.DIR_UP, enable: false, passed: false},
+        {dir: Edge.DIR_RIGHT, in: Edge.DIR_DOWN, enable: false, passed: false},
+      ];
+    }
+    this.withDirs[0].enable = e0;
+    this.withDirs[1].enable = e1;
+  }
+
 }
 
 
@@ -72,8 +97,56 @@ export class Route {
     this.col = 0x000000;
     this.a = 0;
 
-    
+    /** [x,y] の配列 @type {number[][]} */
+    this.pts = [];
   }
+
+  static normal(vs) {
+    let sum = vs[0] ** 2 + vs[1] ** 2;
+    let k = (sum > 0) ? 1 / Math.sqrt(sum) : 0;
+    return [vs[0] * k, vs[1] * k];
+  }
+
+  connectLines() {
+    const ret = [];
+
+    let curStart = this.pts[0];
+    let curEnd = null;
+    let index = 1;
+    /** 最後の方向ベクトル */
+    let dv = [0, 0];
+    while (index < this.pts.length) {
+      curEnd = this.pts[index];
+      dv = Route.normal([curEnd[0] - curStart[0], curEnd[1] - curStart[1]]);
+      if (dv[0] === 0 && dv[1] === 0) {
+        continue;
+      }
+      break;
+    }
+
+    while (index < this.pts.length) {
+      let next = this.pts[index];
+      const nextDv = Route.normal([next[0] - curEnd[0], next[1] - curEnd[1]]);
+      if (nextDv[0] !== 0 || nextDv[1] !== 0) {
+        if (nextDv[0] === dv[0] && nextDv[1] === dv[1]) {
+          // 同じ向き
+          curEnd = [...next];
+        } else {
+          // 異なる向き
+          ret.push(curStart);
+          curStart = [...curEnd];
+          curEnd = [...next];
+          dv = nextDv;
+        }
+      }
+      index += 1;
+    }
+    ret.push(curStart);
+    ret.push(curEnd);
+    console.log('connectLines', ret);
+    return ret;
+  }
+
 }
 
 export class Contour {
@@ -128,7 +201,8 @@ export class Contour {
 
         const dot = new Dot();
         this.dots.push(dot);
-
+        dot.x = j;
+        dot.y = i;
         dot.a = a;
         dot.col = Contour.cstoone(r, g, b);
 
@@ -136,11 +210,13 @@ export class Contour {
           const comp = this.dots(index - 1);
           const edge = dot.ns[Edge.DIR_LEFT];
           edge.canMove = !dot.eqCols(comp);
+          edge.set(Edge.DIR_LEFT, (comp.a !== 0), (dot.a !== 0));
         }
         if (i >= 1) { // 上を見る
           const comp = this.dots(index - w);
           const edge = dot.ns[Edge.DIR_UP];
           edge.canMove = !dot.eqCols(comp);
+          edge.set(Edge.DIR_UP, (comp.a !== 0), (dot.a !== 0));
         }
 
       }
@@ -150,6 +226,7 @@ export class Contour {
 
   
   search() {
+    const ret = [];
     const w = this.width;
     const h = this.height;
     for (let i = 0; i < h; ++i) {
@@ -163,11 +240,31 @@ export class Contour {
             continue;
           }
 
-          // 探し回る
+          for (let l = 0; l < 2; ++l) {
+            const withD = edge.withDirs[l];
+            if (!withD.enable || withD.passed) {
+              continue;
+            }
+
+            // 探し回る
+            const route = new Route();
+            ret.push(route);
+            route.col = dot.col;
+            route.a = dot.a;
+
+            let curWithD = withD;
+            while (true) {
+              curWithD.passed = true;
+
+              break;
+            }
+          }
 
         }
       }
     }
+    console.log('search', ret);
+    return ret;
   }  
 
 }
