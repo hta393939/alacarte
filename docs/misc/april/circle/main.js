05c755f3bfd5fd9d6bc29ca1b12039ae152f99d4
hta393939 <hta393939@gmail.com>
2026-05-09 10:46:38 +0900

    WIP: circle

---
+++ docs/misc/april/circle/main.js
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found at:
+ *  https://github.com/webrtc/samples/blob/gh-pages/LICENSE.md
+ */
+
+// Put variables in global scope to make them available to the browser console.
+const video = window.video = document.getElementById('webcam_canvas');
+const canvas = window.canvas = document.getElementById('out_canvas');
+
+// set camera info
+var cameraInfoBox = document.getElementById('camera_info');
+const cameraInfoDefaults = window.cameraInfo = JSON.parse(cameraInfoBox.value);
+
+canvas.width = 480;
+canvas.height = 360;
+
+const _readyCamera = async () => {
+  const constraints = {
+    audio: false,
+    video: {
+      facingMode: { ideal: 'environment' },
+      width: cameraInfo.img_size[0],
+      height: cameraInfo.img_size[1],
+    }
+  };
+
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    window.stream = stream; // make stream available to browser console
+    video.srcObject = stream;
+  } catch (e) {
+    console.log('navigator.MediaDevices.getUserMedia error: ', e.message, e.name);
+  }
+};
+
+// Change listener for camera parameters
+cameraInfoBox.addEventListener('change', () => {
+  try {
+    window.cameraInfo = JSON.parse(cameraInfoBox.value);
+  } catch (err) {
+    console.log("Error parsing camera parameters!", err);
+    cameraInfoBox.value = JSON.stringify(cameraInfoDefaults, null, 2);
+  }
+});
+
+_readyCamera();
